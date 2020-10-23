86e0077ae591d42dda401227733dfa207f0620bf
Nick Ingram <wningram2@gmail.com>
2020-10-22 21:44:39 -0700

    Initial commit

---
+++ client/client.py
@@ -0,0 +1,11 @@
+import os, sys
+import flask
+
+def main():
+    raise NotImplementedError()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit(0)
